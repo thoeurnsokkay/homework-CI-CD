6be996ab37578a179f3236ba020843f910860778
thoeurnsokkay <thoeurnsokkay@gmail.com>
2026-02-25 02:51:54 +0700

    test ci/cd workflow frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Mail, MapPin, Phone, Bell, User, ArrowRight } from 'lucide-react';
+import Image from 'next/image'; 
+import Link from 'next/link';
 
 export default function ContactPage() {
   return (
